4ab936ebd487ef91a527ccb3d07302f8ea8da1eb
akhnabiev-ai <akhnabiev@gmail.com>
2025-09-16 13:30:56 +0500

    Yangi dars yuklandi
    4-dars qushildi

---
+++ 04-dars/variables.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  5 14:04:39 2025
+@author: Abdurakhmon 
+4-dars
+"""
+
+ism = "Abdurahmon"
+#Ism = "Abdurahmonjon"
+yosh = 38
+print('Mening ismim', ism, 'yoshim',yosh)
+salom = "Hello, WORLD!"
+print(salom)
+xabar = """aloooooooooooooooo Buxoroooooooooooo
+qanday sizlarrrrrrrrrrrr"""
+print(xabar)
+#class=123
+radius = 5
+pi = 3.14
+aylana_yuzi  = pi*radius**2
+print('Radiusi',radius,'aylananing yuzi',aylana_yuzi,'ga teng')
