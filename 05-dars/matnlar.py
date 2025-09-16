d130d1bb1c0accbb77c2f1f7b1b32fdb7ef47fdc
akhnabiev-ai <akhnabiev@gmail.com>
2025-09-16 13:35:11 +0500

    Yangi 5-dars yuklandi
    python 5-dars

---
+++ 05-dars/matnlar.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  5 14:29:44 2025
+
+@author: Abdurakhmon
+5-dars
+"""
+"""matn = "Men yangi 📱 oldim"
+print(matn)
+ism = 'Ahad'
+familiya = 'Qayum'
+print(ism + ' ' + familiya) # ikki o'zgaruvchi orasiga bo'sh joy qo'shamiz
+ism = "Anvar"
+familiya = "Narzulloev"
+ism_sharif = f"{ism} {familiya}"
+print(ism_sharif.upper())
+print(ism_sharif.lower())
+print('Hello World!')
+print('Hello \tWorld!')
+print('Hello \nWorld!')
+ism_sharif = 'james bond'
+print(ism_sharif.capitalize())
+
+meva = "     olmani     "
+print("Men " + meva.lstrip() + " yaxshi ko'raman")
+print("Men " + meva.rstrip() + " yaxshi ko'raman")
+print("Men " + meva.strip() + " yaxshi ko'raman")
+print("Men " + meva + " yaxshi ko'raman")
+
+ism = input('Ismingiz nima?')
+print("Assalomu Alaykum", ism)
+
+ism = input("Ismingiz nima?\n>>>") # Foydalanuvchi ismini yangi qatordan kiritadi
+print("Assalom alaykum, " + ism.title())
+
+"""
+kocha = "Bog'bon"
+mahalla = "Sog'bon"
+tuman = "Bodomzor"
+vil = "Samarqand"
+
+print(kocha,'k\'chasi\n',mahalla,'mahallasi\n',tuman,'tumani\n',vil,'viloyati\n')
+manzil = f"{kocha} ko'chasi,{mahalla} {tuman} {vil}"
+print(manzil.title())
+print(manzil.capitalize())
+print(manzil.upper())
